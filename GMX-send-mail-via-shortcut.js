70d60a3e76faa9bb698a8ff7526b0e7c07cb154e
Sascha Meyer <sascha.meyer@sig.biz>
2016-03-23 15:10:21 +0100

    Updated match parameter

--- GMX-send-mail-via-shortcut.js
+++ GMX-send-mail-via-shortcut.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  hit CTRL+ENTER to send an email when using GMX web version
 // @author       Sascha Meyer
-// @match        https://navigator.gmx.net/navigator/show?sid=7d5ff7a0f8e59903d9754a6e6daa8a542ddac6fe4373e049d77392ab0179b70af40ee8730d0b375417629eb29b0d3b39#mail
+// @match        https://*.gmx.net/mail/client/mail/compose/*
 // @grant        feel free to use, update, share, whatever
 // ==/UserScript==
 /* jshint -W097 */
